c25a39111492fe461a919199d531977594d005d8
klokko <klokko@gmail.com>
2022-01-11 16:07:59 +0000

    Created array with list of all the NFL teams

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //This project selects two random NFL teams, chooses a day of they week they play, and redicts the outcome of thier matchup.
+
+const nflTeams = [
+    'Arizona Cardinals', 'Cincinati Bengals', 'Cleveland Browns', 'Green Bay Packers', 'Jacksonville Jaguars', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions',  'Tennessee Titans', 'Las Vegas Raiders', 'Baltimore Ravens', 'Tampa Bay Buccaneers', 'Miami Dolphins', 'New York Giants', 'New York Jets', 'Carolina Panthers', 'Indianapolis Colts', 'San Francisco Giants', 'Los Angeles Rams', 'Los Angeles Chargers', 'Denver Broncos', 'New Orleans Saints', 'Seattle Seahawks', 'Houston Texans', 'Kansas City Chiefs', 'Washington Football Team', 'Pittsburgh Steelers', 'Philadelphia Eagles', 'New England Patriots', 'Dallas Cowboys', 'Buffalo Bills', 'Atlanta Hawks'
+]
+
+console.log(nflTeams.length);
