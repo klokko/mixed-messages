e526ae953fcc3096f89494dedc2db1c0b92d1874
klokko <klokko@gmail.com>
2022-01-11 16:14:24 +0000

    Added matchDays and outcomes arrays

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const nflTeams = [
     'Arizona Cardinals', 'Cincinati Bengals', 'Cleveland Browns', 'Green Bay Packers', 'Jacksonville Jaguars', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions',  'Tennessee Titans', 'Las Vegas Raiders', 'Baltimore Ravens', 'Tampa Bay Buccaneers', 'Miami Dolphins', 'New York Giants', 'New York Jets', 'Carolina Panthers', 'Indianapolis Colts', 'San Francisco Giants', 'Los Angeles Rams', 'Los Angeles Chargers', 'Denver Broncos', 'New Orleans Saints', 'Seattle Seahawks', 'Houston Texans', 'Kansas City Chiefs', 'Washington Football Team', 'Pittsburgh Steelers', 'Philadelphia Eagles', 'New England Patriots', 'Dallas Cowboys', 'Buffalo Bills', 'Atlanta Hawks'
 ]
 
-console.log(nflTeams.length);
+const matchDays = ['Monday Night Football', 'Thursday Night Football', 'Sunday Night Football', 'Sunday']
+
+const outcomes = ['beat', 'looses to']
