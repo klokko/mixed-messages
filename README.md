449ffd1beb14afa4ca6b03b8d6af1344bf85fa40
klokko <klokko@gmail.com>
2022-01-12 12:13:34 +0000

    Updated the program to predict the scores and indicate a win, loss, or tie

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mixed-messages
-This project selects two random NFL teams, chooses a day of the week they play, and predicts the outcome of the matchup.
+This project selects two random NFL teams, chooses a day of the week they play, and predicts the score of the game.
 
 ## Table of contents
 - General Info
@@ -12,7 +12,7 @@ This project selects two random NFL teams, chooses a day of the week they play, 
 - This project is a portfolio project undertaken as part of the Codecademy full-stack engineer career path. 
 - The project is to build a message generator program that outputs a new randomized output every time the user runs the program.
 - I have chosen to do something that involves sports and the NFL because I have a genuine intreste in the NFL.
-- This project will select 2 of the 32 NFL teams at random and choose a win or loss outcome.
+- This project will select 2 of the 32 NFL teams at random and predict the scores of their game.
 - The project will also select a day of the week for the game that is to be played. It will select between: Monday night, Thursday night, Sunday afternoon, or Sunday night.
 
 ## Technologies Used
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//This project selects two random NFL teams, chooses a day of they week they play, and redicts the outcome of thier matchup.
+//This project selects two random NFL teams, chooses a day of they week they play, and predicts the score of the game.
 
 const nflTeams = [
     'Arizona Cardinals', 'Cincinati Bengals', 'Cleveland Browns', 'Green Bay Packers', 'Jacksonville Jaguars', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions',  'Tennessee Titans', 'Las Vegas Raiders', 'Baltimore Ravens', 'Tampa Bay Buccaneers', 'Miami Dolphins', 'New York Giants', 'New York Jets', 'Carolina Panthers', 'Indianapolis Colts', 'San Francisco Giants', 'Los Angeles Rams', 'Los Angeles Chargers', 'Denver Broncos', 'New Orleans Saints', 'Seattle Seahawks', 'Houston Texans', 'Kansas City Chiefs', 'Washington Football Team', 'Pittsburgh Steelers', 'Philadelphia Eagles', 'New England Patriots', 'Dallas Cowboys', 'Buffalo Bills', 'Atlanta Hawks'
@@ -7,6 +7,16 @@ const nflTeams = [
 const matchDays = ['Monday Night Football', 'Thursday Night Football', 'Sunday Night Football', 'Sunday Afternoon Football']
 
 const outcomes = ['beat', 'loose to']
+
+const scores = [0, 0, 0, 3, 3, 3, 7, 7, 7, 8]
+
+const scorePredictor = () => {
+    let gameScore = [];
+    for (let i = 0; i < 4; i++) {
+        gameScore.push(selectRandom(scores));
+    }
+    return gameScore.reduce((a, b) => a + b, 0);
+}
 
 const selectRandom = array => {
     const randomPosition = Math.floor(Math.random() * array.length);
@@ -19,9 +29,17 @@ const nflGamePrediction = () => {
     while (firstTeam === secondTeam) {
         secondTeam = selectRandom(nflTeams);
     }
-    const gamePrediction = selectRandom(outcomes);
+    const firstTeamScore = scorePredictor();
+    const secondTeamScore = scorePredictor();
+    if (firstTeamScore === secondTeamScore) {
+        gamePrediction = 'tie';
+    } else if (firstTeamScore > secondTeamScore) {
+        gamePrediction = 'beat';
+    } else {
+        gamePrediction = 'loose to';
+    }
     const gameTime = selectRandom(matchDays);
-    console.log(`The ${firstTeam} will ${gamePrediction} the ${secondTeam} on ${gameTime}.`)
+    console.log(`The ${firstTeam} will ${gamePrediction} the ${secondTeam}, ${firstTeamScore}-${secondTeamScore} on ${gameTime}.`)
 }
 
 nflGamePrediction();
